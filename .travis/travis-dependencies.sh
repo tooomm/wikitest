6d23d21b1458917201002332fa14f5bc93de99a8
tooomm <tooomm@users.noreply.github.com>
2018-01-20 17:16:24 +0100

    switch to sudo:false container-based and apt install

--- .travis/travis-dependencies.sh
+++ .travis/travis-dependencies.sh
@@ -5,29 +5,3 @@ if [[ $TRAVIS_OS_NAME == "osx" ]] ; then
   brew install --force qt@5.7
   brew install protobuf
 fi
-if [[ $TRAVIS_OS_NAME == "linux" ]] ; then
-  # common prerequisites
-  sudo add-apt-repository -y ppa:smspillaz/cmake-master
-  sudo apt-get update -qq
-  sudo apt-get -y purge cmake
-  sudo apt-get install -y \
-    libprotobuf-dev protobuf-compiler \
-    cmake \
-    bc \
-    qt5-default qttools5-dev qttools5-dev-tools \
-    qtmultimedia5-dev \
-    libqt5multimedia5-plugins libqt5svg5-dev libqt5sql5-mysql
-	
-  # prerequisites for tests under linux
-  if [[ $BUILDTYPE == "Debug" ]]; then
-    sudo apt-get install -y libgtest-dev
-
-    sudo mkdir /usr/src/gtest/build
-    cd /usr/src/gtest/build
-    sudo cmake .. -DBUILD_SHARED_LIBS=1 -DRULE_LAUNCH_COMPILE=ccache --debug-output
-    sudo make -j2
-    sudo ln -s /usr/src/gtest/build/libgtest.so /usr/lib/libgtest.so
-    sudo ln -s /usr/src/gtest/build/libgtest_main.so /usr/lib/libgtest_main.so
-    cd -
-  fi
-fi
