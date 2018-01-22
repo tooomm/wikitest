a6c493e0327980d4729137751fc49c043b07f60c
tooomm <tooomm@users.noreply.github.com>
2018-01-22 15:09:47 +0100

    Revert "test2"
    This reverts commit 2b6ec1155384fe78a5403513c36475a2b853589c.

--- .travis/travis-compile.sh
+++ .travis/travis-compile.sh
@@ -18,10 +18,10 @@ fi
 
 if [[ $BUILDTYPE == "Debug" ]]; then
 	cmake .. -DWITH_SERVER=1 -DTEST=1 -DCMAKE_BUILD_TYPE=$BUILDTYPE $prefix
-	make -j4
+	make -j2
 	make test
 fi
 if [[ $BUILDTYPE == "Release" ]]; then
 	cmake .. -DWITH_SERVER=1 -DCMAKE_BUILD_TYPE=$BUILDTYPE $prefix
-	make package -j4
+	make package -j2
 fi
